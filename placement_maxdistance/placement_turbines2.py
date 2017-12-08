311902f5d4ff623cab4cdad243164217be56bc94
Sebastian <s.sanchezperezmoreno@tudelft.nl>
2017-12-08 14:41:53 -0700

    IEA

--- placement_maxdistance/placement_turbines2.py
+++ placement_maxdistance/placement_turbines2.py
@@ -4,7 +4,7 @@ from matplotlib.path import Path
 import matplotlib.patches as patches
 from time import time
 from PNPOLY import pnpoly
-from math import sqrt
+from math import sqrt, floor, ceil
 
 from ray_crossing_polygon_test import Point, Polygon
 def place_turbines():
@@ -12,24 +12,25 @@ def place_turbines():
                  Point(423974., 6151447.),
                  Point(429014.,	6151447.),
                  Point(429431.,	6147543.)])
-
-    r = Polygon([Point(0., 0.),
-                 Point(0., 100.),
-                 Point(100., 100.),
-                 Point(100., 0.)])
+# 484178.55, 5732482.8], [500129.9, 5737534.4], [497318.1, 5731880.24], [491858.00, 5725044.75]], [[491858.00, 5725044.75], [497318.1, 5731880.24], [503163.37, 5729155.3], [501266.5, 5715990.05
+    r = Polygon([Point(484178.55, 5732482.8),
+                 Point(500129.9, 5737534.4),
+                 Point(497318.1, 5731880.24),
+                 Point(503163.37, 5729155.3), 
+                 Point(501266.5, 5715990.05)])
 
     # Test 1: Point inside of polygon
     # p1 = Point(90, 118)
     # print q.contains(p1)
 
-    nvert = 4
-    vertx = [424386., 423974, 429014., 429431.]
-    verty = [6147543., 6151447., 6151447., 6147543.]
+    nvert = 5
+    vertx = [484178.55, 500129.9, 497318.1, 503163.37, 501266.5]
+    verty = [5732482.8, 5737534.4, 5731880.24, 5729155.3, 5715990.05]
 
     inside = []
-    # plot_data = open("data.dat", "w")
-    for i in range(423974, 429431, 80):
-        for j in range(6147543, 6151447, 80):
+    plot_data = open("data.dat", "w")
+    for i in range(int(floor(min(vertx))), int(ceil(max(vertx))), 10):
+        for j in range(int(floor(min(verty))), int(ceil(max(verty))), 10):
             # a = q.contains(Point(i, j))
             if pnpoly(nvert, vertx, verty, i, j):
                 inside.append((i, j))
@@ -46,7 +47,7 @@ def place_turbines():
     points = [inside[h]]
     # plot_data.write("{0:d}\t{1:d}\t{2:d}\n".format(points[-1][0], points[-1][1], 0))
 
-    for nt in range(1, 80):
+    for nt in range(1, 74):
         dist = []
         i = 0
         for item in inside:
@@ -59,7 +60,7 @@ def place_turbines():
             i += 1
 
         next = inside[max(dist)[1]]
-        # plot_data.write("{0:d}\t{1:d}\t{2:d}\n".format(next[0], next[1], nt))
+        plot_data.write("{0}\t{1}\t{2}\n".format(next[0], next[1], nt))
         points.append(next)
         inside.remove(next)
 
@@ -70,12 +71,11 @@ def place_turbines():
 if __name__ == '__main__':
     points = place_turbines()
 
-    verts = [(424386., 6147543.),
-             (423974., 6151447.),
-             (429014., 6151447.),
-             (429431., 6147543.),
-             (424386., 6147543.)
-             ]
+    verts = [(484178.55, 5732482.8),
+                 (500129.9, 5737534.4),
+                 (497318.1, 5731880.24),
+                 (503163.37, 5729155.3), 
+                 (501266.5, 5715990.05)]
 
     codes = [Path.MOVETO,
              Path.LINETO,
@@ -90,8 +90,8 @@ if __name__ == '__main__':
     ax = fig.add_subplot(111)
     patch = patches.PathPatch(path, facecolor='orange', lw=2)
     ax.add_patch(patch)
-    ax.set_xlim(423870, 429531)
-    ax.set_ylim(6147440, 6151547)
+    ax.set_xlim(484178.55, 503163.37)
+    ax.set_ylim(5715990.05, 5737534.4)
     toplot_x, toplot_y = zip(*points)
     plt.plot(toplot_x, toplot_y, 'ro')
     plt.show()
